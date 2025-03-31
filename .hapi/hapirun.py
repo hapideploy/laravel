e96bd274d4140d032f56f154907b9766e6f86aba
Quynh Nguyen <seriquynh@gmail.com>
2025-03-31 11:59:51 +0700

    Use .hapi directory

--- .hapi/hapirun.py
+++ .hapi/hapirun.py
@@ -12,8 +12,5 @@ app.add('shared_dirs', [])
 app.add('shared_files', [])
 app.add('writable_dirs', [])
 
-app.put('writable_mode', 'chmod')
-app.put('writable_chmod_mode', '0775')
-
 if __name__ == '__main__':
     app.start()
