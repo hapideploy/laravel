8b6a641e47ea86b5e140e64a3e864d2b48157290
Quynh Nguyen <seriquynh@gmail.com>
2025-04-16 16:15:26 +0700

    Update hapi stuff

--- .hapi/deploy.py
+++ .hapi/deploy.py
@@ -1,16 +1,13 @@
-from hapi.toolbox import app
+from hapi.cli import app
 
 from hapi.recipe.laravel import Laravel
 
 app.load(Laravel)
 
-app.put('name', 'Laravel')
+app.put('name', 'laravel')
 app.put('repository', 'https://github.com/hapideploy/laravel')
 app.put('branch', 'main')
 
 app.add('shared_dirs', [])
 app.add('shared_files', [])
 app.add('writable_dirs', [])
-
-if __name__ == '__main__':
-    app.start()
