45764cc184d319d86c696a67b62ad459c3fe53dc
Quynh Nguyen <seriquynh@gmail.com>
2025-04-16 14:46:55 +0700

    Test it does not need to restart PHP FPM

--- routes/web.php
+++ routes/web.php
@@ -11,5 +11,9 @@ Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/sample', function () {
+    return 'This is a sample route.';
+});
+
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
